98c0420a87f9a78232f985dc41dc4e610f122232
vandu <vandu@DESKTOP-CKTBGDM>
2024-08-05 22:25:16 +0700

    gg

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import os
 import google.generativeai as genai
 import discord
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+genai.configure(api_key=os.environ["GEN_KEY"])
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 chat = model.start_chat(history=[])
 
 content = " "
 
-def response_generate():
-    content = input("your turn: ")
+def response_generate(content):
     response = chat.send_message(content)
-    print(response.text)
+    return response.text
 
-DISCORD_KEY = os.environ["DISCORD_BOT_API_KEY"]
+DISCORD_KEY = os.environ(os.environ["DIS_KEY"])
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 @client.event
 async def on_message(message):
-        await message.channel.send(response_generate())
+    if message.author == client.user:
+        return
+    if message.channel.id == 1270035200100925510 and message.content.startswith("ask"):
+        await message.channel.send(response_generate(message.content))
 client.run(DISCORD_KEY)
